b74453432744a0191a3a0fea33045fae4dd7d700
Joe Robbins <joe.robbins.cpt@gmail.com>
2023-03-12 17:28:04 -0400

    added services page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import { useHistory } from "react-router-dom";
+import Services from './Services';
+
+
+
+
 function Home() {
     const history = useHistory()
 
@@ -7,16 +12,21 @@ function Home() {
 
 
     return (
-    <div> 
-        <h1 className="text-xl font-bold md:text-2xl items-center">
-        Allright Computer Services
-        </h1>
+    <div > 
+        <div className='flex flex-row items-center ml-16'>
+            <h1 className="text-xl font-bold md:text-2xl">
+            Allright Computer Services
+            </h1>
+        </div>
 
-        <div className='container flex flex-row items-center font-bold'>
+        <div className='container flex flex-row items-center font-bold ml-16'>
             <a className='mx-5 hover:text-blue-500 transition duration-200' onClick={() => history.push('/about')}>About</a>
             <a className='mx-5 hover:text-blue-500 transition duration-200'>Services</a>
             <a className='mx-5 hover:text-blue-500 transition duration-200'>Contact</a>
         </div>
+
+        <Services />
+
 
     </div>
     )
---
+++ src/components/Services.js
@@ -0,0 +1,76 @@
+import { useState } from "react"
+import React from 'react'
+
+function Services() {
+
+    return (
+        <div>
+
+
+
+    <div class=" grid xl:grid-cols-3 md:grid-cols-2 gap-12 mt-20 justify-center">
+
+        <div class=" p-4 w-96 h-96 sm:h-auto text-xl border-4 border-cyan-800 rounded-xl">  
+
+            <p className='font-bold mb-8 font-mono'>Spyware / Virus Removal</p>
+            
+            <div>
+                <p className="text-base font-sans">Are you struggling with a slow or infected computer? I can help! As an expert technician, I will thoroughly scan your device to detect and remove any malicious software that may be causing issues. I use state-of-the-art tools and techniques to ensure that all malware is completely eliminated, including spyware, viruses, adware, and more. I'll also provide tips on how to protect your computer in the future, so you can avoid similar problems down the road. My goal is to get your computer running like new again, without any annoying pop-ups, crashes, or other issues. </p>
+            </div>
+        </div>
+        
+        <div class=" p-4 w-96 h-96 sm:h-auto text-xl border-4 border-cyan-800 rounded-xl">  
+
+            <p className='font-bold mb-8 font-mono'>Desktop / Laptop Repair</p>
+            
+            <div>
+                <p className="text-base font-sans">If your device is running slow or experiencing frequent crashes, I can help! I will conduct a comprehensive diagnostic test on your device to identify the root cause of any issues. Using state-of-the-art tools and techniques, I can repair your device including hardware repairs, software repairs, virus removal, and more. My goal is to get your computer running like new again, so you can work efficiently and without interruption. Additionally, I will provide you with tips on how to maintain your device. </p>
+            </div>
+        </div>
+
+        <div class=" p-4 w-96 h-96 sm:h-auto text-xl border-4 border-cyan-800 rounded-xl">  
+
+            <p className='font-bold mb-8 font-mono'>Data Recovery</p>
+            
+            <div>
+                <p className="text-base font-sans">Need to recover lost data? I will use proven techniques to recover lost or corrupted data from your device. Whether it's due to accidental deletion, virus attacks, or hardware failures, I have the skills and expertise to restore your data. With years of experience, I understand the importance of your data and will ensure that all recovered data is kept confidential and secure.</p>
+            </div>
+        </div>
+
+        <div class=" p-4 w-96 h-96 sm:h-auto text-xl border-4 border-cyan-800 rounded-xl">  
+
+            <p className='font-bold mb-8 font-mono'>Hard Drive Replacement</p>
+            
+            <div>
+                <p className="text-base font-sans">Are you experiencing issues with your hard drive? If your hard drive is failing or you're in need of an upgrade, I'm here to help with my hard drive replacement service! I provide a fast and efficient hard drive replacement service for all types of devices, including desktops, laptops, and servers. I'll carefully install a new hard drive and transfer your data, ensuring that all your important files are safe and secure. I use high-quality hard drives from trusted brands to ensure the best performance and longevity for your device.</p>
+            </div>
+        </div>
+
+        <div class=" p-4 w-96 h-96 sm:h-auto text-xl border-4 border-cyan-800 rounded-xl">  
+
+            <p className='font-bold mb-8 font-mono'>Windows Installation</p>
+            
+            <div>
+                <p className="text-base font-sans">I provide a fast and reliable service to install or upgrade your computer's operating system to the latest version of Microsoft Windows. Whether you're looking to upgrade from an older version or need a fresh installation, I can ensure a seamless transition to the latest version of Windows. I'll also help you set up your device with the latest security updates, drivers, and software, so you can get the most out of your device. </p>
+            </div>
+        </div>
+
+        <div class=" p-4 w-96 h-96 sm:h-auto text-xl border-4 border-cyan-800 rounded-xl">  
+
+            <p className='font-bold mb-8 font-mono'>Performance Upgrades</p>
+            
+            <div>
+                <p className="text-base font-sans">Is your computer running slow or struggling to keep up with demanding applications? I can help with my computer performance upgrade service! I provide a range of performance upgrades to boost your computer's speed and overall performance. From increasing your device's RAM to upgrading its storage or graphics capabilities, I'll help you identify the best upgrades to meet your needs and budget. I use high-quality components from trusted brands to ensure the best performance and longevity for your device. I can also help you optimize your device's settings and remove unnecessary software, further improving its performance.</p>
+            </div>
+        </div>
+
+        
+
+        
+    </div>
+
+        </div>
+    )
+}
+
+export default Services
